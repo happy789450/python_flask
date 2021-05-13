31d6c26d059c70649f15f9b73f227b65f2b5456f
Your Name <you@example.com>
2021-05-14 00:05:17 +0800

    登录系统

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 import requests
 from flask import request
 from flask import Flask
@@ -8,9 +9,12 @@ from views import views
 from sersers import servers
 from auth import auth
 from flask import redirect
+from flask import session
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '123456'
+app.permanent_session_lifetime=datetime.timedelta(minutes=60)
 app.register_blueprint(shijiange, url_prefix="/shijiange")
 app.register_blueprint(rice, url_prefix="/rice")
 app.register_blueprint(views, url_prefix="/views")
@@ -22,12 +26,15 @@ app.register_blueprint(auth, url_prefix="/auth")
 def index():
     return render_template("test/index.html")
 
+
 @app.before_request
 def before_request():
-    if request.path == "/static/login.html" or request.path =="/auth/login" or request.path.endswith(".js") or request.path.endswith(".css"):
+    if request.path == "/static/login.html" or request.path == "/auth/login" or request.path.endswith(
+            ".js") or request.path.endswith(".css"):
         pass
     else:
-        username = request.cookies.get('username')
+        # username = request.cookies.get('username')
+        username = session.get('username')
         if not username:
             return redirect('/static/login.html')
 
@@ -38,4 +45,4 @@ def hello_user2():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5500')
+    app.run(host='0.0.0.0', port='5600')
--- auth.py
+++ auth.py
@@ -1,9 +1,11 @@
 import json
 import os
+import tool_db
 from flask import Blueprint
 from flask import request
 from flask import redirect
 from flask import make_response
+from flask import session
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
 
@@ -14,15 +16,24 @@ auth = Blueprint("auth", __name__)
 def login():
     username = request.form.get('username')
     password = request.form.get('password')
-    if username == "rice" and password == "123456":
-        response = make_response(redirect("/views/servers"))
-        response.set_cookie('username',username,max_age=86400)
-        return response
+    sql = "select * from user where username = %s and password = %s"
+    result = tool_db.updateByParameters(sql, (username, password))
+    print(result)
+    if result:
+        session['username'] = username
+        return redirect('/views/servers')
+        # response = make_response(redirect("/views/servers"))
+        # response.set_cookie('username',username,max_age=86400)
+        # return response
     else:
         return redirect("/static/login.html")
 
+
 @auth.route('/logout')
 def logout():
-    response = make_response(redirect("/static/login.html"))
-    response.delete_cookie('username')
-    return response
+    if 'username' in session:
+        del session['username']
+    return redirect("/static/login.html")
+    # response = make_response(redirect("/static/login.html"))
+    # response.delete_cookie('username')
+    # return response
